a4f36afe34a268f1b5f640e23e6753b1b4f1a10e
vitalcha <vkrokhov@gmail.com>
2019-11-20 16:24:20 +0200

    add buld

---
+++ build/precache-manifest.1492b16c5045cb87829f796e2d3f0615.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b4dafd27643c2d0de451b291d6cf34",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "0b4c369ed4e09b10beb4",
+    "url": "/goit-react-hw-01-components/static/css/main.b44b73d6.chunk.css"
+  },
+  {
+    "revision": "7ebff5d1ddb131cd4c84",
+    "url": "/goit-react-hw-01-components/static/js/2.7d6c7229.chunk.js"
+  },
+  {
+    "revision": "0b4c369ed4e09b10beb4",
+    "url": "/goit-react-hw-01-components/static/js/main.6b7b94bd.chunk.js"
+  },
+  {
+    "revision": "b13bd8506889449d2260",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.a64c8a8f.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Header/index.module.css";
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
